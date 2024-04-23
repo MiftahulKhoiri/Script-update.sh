fcce66f762e476f13523a6a326cff8dddd253233
Miftahul Khoiri ("bagusaku") <48151452+MiftahulKhoiri@users.noreply.github.com>
2024-04-23 16:47:01 +0800

    Update loading.sh

--- loading.sh
+++ loading.sh
@@ -5,7 +5,7 @@ LOADING="[..................................................]"
 icon="="
   for i in $(seq 1 100); do
       LOADING=${LOADING/./$icon} 
-      sleep 0.50
+      sleep 0.10
       clear
       echo $LOADING $i"%"
   done
